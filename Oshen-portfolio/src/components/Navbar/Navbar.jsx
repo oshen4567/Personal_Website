0cc62378bd3b31190363720b493ec99f7bfcafc1
oshen4567 <COHDSE221F-013@student.nibm.lk>
2023-10-03 10:05:09 +0530

    Comit 2 Day 1
    Functionality of NavBar

--- Oshen-portfolio/src/components/Navbar/Navbar.jsx
+++ Oshen-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,29 @@
 import React, {useState}from 'react';
 
 import styles from "./Navbar.module.css"
-import {getImage} from "../../utils"
+import {getImageUrl} from "../../utils"
 
 const Navbar = () => {
+  const [menuOpen , setMenuOpen] =useState(false);
   return (
     <nav className={styles.navbar}>
         <a className={styles.title} href="/">OshenTheDev</a>
         <div className= {styles.menu}>
-            <ul className={styles.menuItems}>
+          <img className= {styles.menuBtn}
+           src={
+            menuOpen 
+            ? getImageUrl ("nav/closeIcon.png")
+            : getImageUrl ("nav/menuIcon.png")
+            
+          } 
+           alt="menu-button"
+           onClick={()=> setMenuOpen(!menuOpen)}
+           />
+          
+            <ul
+               className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+               onClick={() => setMenuOpen(false)}
+            >
                 <li><a href="#about">About</a></li>
                 <li><a href="#experience">Experience</a></li>
                 <li><a href="#projects">Projects</a></li>
